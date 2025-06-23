b7ae6d94004ce6d7e4040601d7121cee9335374f
Abinaya141 <abusha1410@gmail.com>
2025-06-23 12:04:32 +0530

    Images added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,17 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+// components
+import { Weather } from "~/components/weather-app";
+
 export default component$(() => {
-  return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
-    </>
-  );
+	return (
+		<div>
+			<Weather />
+		</div>
+	);
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+	title: 'Qwik Weather App',
 };
